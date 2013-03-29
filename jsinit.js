2097d339280a3588ca159f3d990989e0cffb6c88
johorst <johorst@mailinator.com>
2013-03-29 11:03:25 +0100

    Update jsinit.js
    sdfsdf

--- jsinit.js
+++ jsinit.js
@@ -6,3 +6,11 @@ for (var i = 0; i< x.length; i++)
 // besser: $('tr').find('td')
 //auch nicht schlecht: $('tr').find('td')[1].innerHTML.slice(x+7,x+40)
 // for (var i = 5; i< t.length; i++){var k = t[i]; console.log(k.getElementsByTagName('td'))} //noch  besser
+
+for (var i = 0; i< t.length; i++)
+ {var k = t[i]; var di = k.getElementsByTagName('td'); 
+ for (var ei = 0; ei< di.length; ei++)
+ {
+ if(di[ei] != null && di[ei].innerText.match('geo'))
+ {console.log('we got a geo')}}
+ }
