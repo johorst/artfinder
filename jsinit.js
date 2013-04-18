c76a914ad4327626f78e5d6b866b3683d17cc850
johorst <johorst@mailinator.com>
2013-04-18 12:59:10 +0300

    Update jsinit.js
    Versuch, jeden Datensatz nach localhost zu posten.
    Pumb up da Collection!

--- jsinit.js
+++ jsinit.js
@@ -1,36 +1,47 @@
 t = $('tr');
 //console.log(t.length + " anzahl elemente in t")
-var Name, Beschreibung, Params_raw, ParamN, ParamE, Output;
+var Name, Beschreibung, Params_raw, llN, llE, Output, allParams;
 for (var i = 0; i< t.length; i++)
  {var k = t[i]; var di = k.getElementsByTagName('td');
  Name = "";
  Beschreibung = "";
  Params_raw = "";
- ParamN ="";
- ParamE ="";
+ llN ="";
+ llE ="";
  Output = "";
  for (var ei = 0; ei< di.length - 1; ei++)
  {
   var jo = di[ei];
   if(jo.textContent.length > 30)
-  {Beschreibung += " " + jo.textContent.replace("(♁Standort)","");}
-  else{Name += " " + jo.textContent.replace("(♁Standort)","");}
+  {Beschreibung += " " + jo.textContent.replace("(?Standort)","");}
+  else{Name += " " + jo.textContent.replace("(?Standort)","");}
   if(di[ei].innerHTML.match('geo'))
    {
    var c = di[ei].innerHTML.indexOf('params');
    Params_raw = di[ei].innerHTML.slice(c+7,di[ei].innerHTML.length);
    Params_raw = Params_raw.slice(0, Params_raw.indexOf('E'));
-   ParamN = Params_raw.slice(0,Params_raw.indexOf('N') - 1);
-   ParamE = Params_raw.slice(Params_raw.indexOf('N') + 2, Params_raw.length-1);
+   llN = Params_raw.slice(0,Params_raw.indexOf('N') - 1);
+   llE = Params_raw.slice(Params_raw.indexOf('N') + 2, Params_raw.length-1);
 //   Params = di[ei].innerHTML.slice(c+7,c+30);   
    }    
  }
  if(Beschreibung == "")
- {console.log("Name: " + Name +  "   " + "; Params: " + ParamN + "    " + ParamE);}
- else{console.log("Name: " + Name +  "   " + "; Beschreibung: " + Beschreibung + "; Params: " + ParamN + "    " + ParamE);}
- console.log("https://maps.google.de/maps?t=h&z=21&layer=c&ll="+ParamN+","+ParamE);
- console.log(Params_raw);
+ {console.log("Name: " + Name +  "   " + "; Params: " + llN + "    " + llE);}
+ else{console.log("Name: " + Name +  "   " + "; Beschreibung: " + Beschreibung + "; Params: " + llN + "    " + llE);}
+ console.log("https://maps.google.de/maps?t=h&z=21&layer=c&ll="+llN+","+llE);
+ jQuery.post( 'http://localhost:4711/api/books', {
+    'title': Name,
+    'author': Beschreibung,
+    'releaseDate': new Date( 2008, 4, 1 ).getTime(),
+ 'llN': llN,
+	'llE': llE
+ }, function(data, textStatus, jqXHR) {
+    console.log( 'Post response:' );
+    console.dir( data );
+    console.log( textStatus );
+    console.dir( jqXHR );
+ });
+ 
 }
-
 jo: das die map die referenziert werden soll (letzter Parameter 0,0,0,0 passt scho - zumindest bekommt man so die Street View hin)
 https://maps.google.de/maps?t=h&z=21&layer=c&cbll=48.137572,11.462496&cbp=0,0,0,0
